2ceec2f2c7bbee0bc2b57915249862f02ebf5cac
tharindu99 <madushanka.best@gmail.com>
2017-05-15 13:52:44 +0530

    firebase initialized

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var admin = require('firebase-admin');
 var express = require('express');
 var app = express();
 
@@ -16,5 +17,24 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+var serviceAccount = require("./key/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://muthubange.firebaseio.com"
+});
+
+function writeUserData(userId, name, email) {
+  console.log("call for writeuserdata");
+
+  admin.database().ref('users/' + userId).set({
+    username: name,
+    email: email
+  });
+}
+
+writeUserData("111","test user1","afatahudu.com");
+
 
 
